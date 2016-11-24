3ce0cd9a951cb868c62a52c859436e99525104af
MSitarz <1507342@Y348SDP08.ad.dundeeandangus.ac.uk>
2016-11-24 09:27:50 +0000

    1.1 version of whatever x)

--- src/firstapp/FirstApp.java
+++ src/firstapp/FirstApp.java
@@ -17,7 +17,7 @@ public class FirstApp {
     public static void main(String[] args) {
         // TODO code application logic here
         // a few changes :P
-        
+        System.out.println("Hi");
     }
     
 }
